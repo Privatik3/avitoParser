8302f1c9aadadbad1cd5805b2cabba65a3e1b14a
Privatik3 <lol11999333@gmail.com>
2018-08-20 14:39:50 +0300

    Фильтер для отчёта ( доп. поля )

---
+++ src/main/java/google/ReportFilter.java
@@ -0,0 +1,68 @@
+package google;
+
+public class ReportFilter {
+
+    private boolean photo;
+    private boolean description;
+    private boolean descriptionLength;
+    private boolean sellerName;
+    private boolean position;
+    private boolean date;
+    private boolean phone;
+
+    public boolean isPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(boolean photo) {
+        this.photo = photo;
+    }
+
+    public boolean isDescription() {
+        return description;
+    }
+
+    public void setDescription(boolean description) {
+        this.description = description;
+    }
+
+    public boolean isDescriptionLength() {
+        return descriptionLength;
+    }
+
+    public void setDescriptionLength(boolean descriptionLength) {
+        this.descriptionLength = descriptionLength;
+    }
+
+    public boolean isSellerName() {
+        return sellerName;
+    }
+
+    public void setSellerName(boolean sellerName) {
+        this.sellerName = sellerName;
+    }
+
+    public boolean isPosition() {
+        return position;
+    }
+
+    public void setPosition(boolean position) {
+        this.position = position;
+    }
+
+    public boolean isDate() {
+        return date;
+    }
+
+    public void setDate(boolean date) {
+        this.date = date;
+    }
+
+    public boolean isPhone() {
+        return phone;
+    }
+
+    public void setPhone(boolean phone) {
+        this.phone = phone;
+    }
+}
